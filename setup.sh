44ae8a7c108cc51b0bffc74f759592d3850b4155
Scott Cressi <scottcressi@gmail.com>
2019-06-01 18:04:26 -0400

    better dotfiles

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo apt-get install -y python-pip
 sudo pip install dotfiles
-dotfiles --repo ~/repos/personal/dotfiles/ --sync --force
+dotfiles --repo ~/repos/personal/dotfiles/ -C ~/repos/personal/dotfiles/.dotfilessrc --sync --force
