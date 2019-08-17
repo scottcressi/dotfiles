ca8cd2eea80796d7f9ea3dcff3d93d6298c065cd
Scott Cressi <scottcressi@gmail.com>
2019-08-17 12:51:58 -0400

    python

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y python-pip
-sudo pip install --upgrade dotfiles
+sudo apt-get install -y python3-pip
+sudo pip3 install --upgrade dotfiles
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 dotfiles --repo "$DIR" -C "$DIR"/.dotfilessrc --sync
