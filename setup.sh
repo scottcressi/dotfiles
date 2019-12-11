b95d6f862f0f7b55eab9aa4f352cd224f31661be
Scott Cressi <scottcressi@gmail.com>
2019-12-11 12:09:08 -0500

    deprecate dotfiles

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y python3-pip
-sudo pip3 install --upgrade dotfiles
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-dotfiles --repo "$DIR" -C "$DIR"/.dotfilessrc --sync
+sudo apt-get install -y --quiet --quiet stow
+stow -v -S -t ~/ .
