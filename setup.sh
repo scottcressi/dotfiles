68c965149e7b3a211e4f38a852d901d5c06ec71f
Scott Cressi <scottcressi@gmail.com>
2019-12-13 10:49:40 -0500

    no wrap

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
 dir=$(basename "$PWD")
-if [ "$dir" == "dotfiles" ] ; then
-
-    if [ -f /etc/debian_version ] ; then
-        package_manager=apt-get
-    else
-        package_manager=yum
-    fi
-
-    sudo $package_manager install -y --quiet --quiet stow
-    stow --verbose --stow --target ~/ .
-
-else
+if [ "$dir" != "dotfiles" ] ; then
     echo please enter dotfiles dir
+    exit 0
 fi
+
+if [ -f /etc/debian_version ] ; then
+    package_manager=apt-get
+else
+    package_manager=yum
+fi
+
+sudo $package_manager install -y --quiet --quiet stow
+stow --verbose --stow --target ~/ .
