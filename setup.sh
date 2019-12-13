ec764d5ac0feae9c40988a6368525b93b14ae70e
Scott Cressi <scottcressi@gmail.com>
2019-12-13 10:41:29 -0500

    dir detection

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-if [ -f /etc/debian_version ] ; then
-    package_manager=apt-get
-else
-    package_manager=yum
-fi
+dir=$(basename "$PWD")
+if [ "$dir" == "dotfiles" ] ; then
 
-sudo $package_manager install -y --quiet --quiet stow
-stow --verbose --stow --target ~/ .
+    if [ -f /etc/debian_version ] ; then
+        package_manager=apt-get
+    else
+        package_manager=yum
+    fi
+
+    sudo $package_manager install -y --quiet --quiet stow
+    stow --verbose --stow --target ~/ .
+
+else
+    echo please enter dotfiles dir
+fi
